14d2bcfaca250f411cadaeb1246754dc377c8856
Atlantiks <atlantiks@bigmir.net>
2021-04-08 00:23:38 +0300

    Added ParserTest

---
+++ NameParserTest.java
@@ -0,0 +1,10 @@
+class NameParserTest {
+
+    public static void main(String[] args) {
+        String[] names = {"John Doe", "Bill Mask", "Nigua Joshua"};
+
+        //John, Bill, Nigua
+        System.out.println(new NameParser().parse(names));
+    }
+
+}
