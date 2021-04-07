4f3b3a3c0dd02dabbadca30acdd4813e30ff8c1f
Atlantiks <atlantiks@bigmir.net>
2021-04-08 00:07:38 +0300

    Initial commit

---
+++ NameParser.java
@@ -0,0 +1,20 @@
+package homework6;
+
+public class NameParser {
+
+    public String parse(String[] names) {
+        StringBuilder result = new StringBuilder("");
+
+        for(int i = 0; i < names.length; i++) {
+            String firstName = names[i].split(" ")[0];
+
+            result.append(firstName);
+            if (i != names.length - 1) {
+                result.append(", ");
+            }
+        }
+
+        return result.toString();
+    }
+
+}
