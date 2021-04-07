72d874856dba5e6d6bf3d0a2907c3c6f207ba314
Atlantiks <atlantiks@bigmir.net>
2021-04-08 00:38:40 +0300

    Classes rearrenged in a single file

--- NameParserTest.java
+++ NameParserTest.java
@@ -8,3 +8,22 @@ public class NameParserTest {
     }
 
 }
+
+class NameParser {
+
+    public String parse(String[] names) {
+        StringBuilder result = new StringBuilder("");
+
+        for(int i = 0; i < names.length; i++) {
+            String firstName = names[i].split(" ")[0];
+
+            result.append(firstName);
+            if (i != names.length - 1) {
+                result.append(", ");
+            }
+        }
+
+        return result.toString();
+    }
+
+}
